4c49dc5381613c64b434b79cc742232810794b86
KaiHang Zhao <>
2019-09-27 09:19:41 +0800

    WebSiteTest

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # THIS IS A TEST
+
+Try an easy "Tic-Tac_Toe" game [here](./Tic-Tac_Toe).
---
+++ Tic-Tac-Toe/precache-manifest.c990a5ece2a0d7d462b7e5343fb32f44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93bb2ba39e69b5fcf6ab3f51ede077a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b976295f7d770e46826",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "4460429f977cd1818ebb",
+    "url": "/static/js/2.464df444.chunk.js"
+  },
+  {
+    "revision": "7b976295f7d770e46826",
+    "url": "/static/js/main.aceab05c.chunk.js"
+  },
+  {
+    "revision": "e3831bc56b6faa02b209",
+    "url": "/static/js/runtime-main.f949f7e3.js"
+  }
+]);
